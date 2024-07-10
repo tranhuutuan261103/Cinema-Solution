645a540ba21043b984b997e430569ca8dee1b639
tranhuutuan261103 <tranhuutuan26112003@gmail.com>
2024-07-10 22:32:03 +0700

    feat: [Cinema Solution] Get all categories

---
+++ CinemaSolution.Application/Category/ICategoryService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CinemaSolution.ViewModels.Category;
+
+namespace CinemaSolution.Application.Category
+{
+    public interface ICategoryService
+    {
+        Task<List<CategoryViewModel>> GetAllCategories();
+    }
+}
---
+++ CinemaSolution.BackendApi/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CinemaSolution.Application.Category;
+
+namespace CinemaSolution.BackendApi.Controllers
+{
+    [ApiController]
+    [Route("categories")]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+        private readonly ILogger<CategoryController> _logger;
+        public CategoryController(ICategoryService categoryService, ILogger<CategoryController> logger)
+        {
+            _categoryService = categoryService;
+            _logger = logger;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _categoryService.GetAllCategories();
+            return Json(categories);
+        }
+    }
+}
