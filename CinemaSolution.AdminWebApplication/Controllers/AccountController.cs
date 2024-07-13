24934c8ef861cba2df8e310eb2bc174967f813a0
tranhuutuan261103 <tranhuutuan26112003@gmail.com>
2024-07-14 00:14:28 +0700

    feat: [Cinema Solution] Views for authenticating

---
+++ CinemaSolution.AdminWebApplication/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaSolution.AdminWebApplication.Controllers
+{
+    [Route("account")]
+    public class AccountController : Controller
+    {
+        [HttpGet("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet("register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpGet("forgot-password")]
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+    }
+}
