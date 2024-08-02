c4f77ddee5955081aee297a1f0659545646e9e0d
tranhuutuan261103 <tranhuutuan26112003@gmail.com>
2024-08-02 23:52:33 +0700

    feat: [Cinema Solution] Get all screenings

---
+++ CinemaSolution.AdminWebApplication/Controllers/ScreeningController.cs
@@ -0,0 +1,33 @@
+﻿using CinemaSolution.AdminWebApplication.Filters;
+using CinemaSolution.Application.Screening;
+using CinemaSolution.ViewModels.Screening;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaSolution.AdminWebApplication.Controllers
+{
+    [Route("screenings")]
+    [Authorize(Roles = "Administrator")]
+    [SetBarActive("screening")]
+    public class ScreeningController : Controller
+    {
+        private readonly IScreeningService _screeningService;
+        public ScreeningController(IScreeningService screeningService)
+        {
+            _screeningService = screeningService;
+        }
+
+        [HttpGet("")]
+        public async Task<IActionResult> Index(int PageIndex = 1, int PageSize = 5, int? movieId = null)
+        {
+            var request = new GetScreeningPagingRequest
+            {
+                PageIndex = PageIndex,
+                PageSize = PageSize,
+                MovieId = movieId
+            };
+            var screenings = await _screeningService.GetPagedResult(request);
+            return View(screenings);
+        }
+    }
+}
---
+++ CinemaSolution.Application/Screening/IScreeningService.cs
@@ -0,0 +1,15 @@
+﻿using CinemaSolution.ViewModels.Common.Paging;
+using CinemaSolution.ViewModels.Screening;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaSolution.Application.Screening
+{
+    public interface IScreeningService
+    {
+        Task<PagedResult<ScreeningViewModel>> GetPagedResult(GetScreeningPagingRequest request);
+    }
+}
