13bfbe08c6d3f139303b3e01fc5316bf389ae8a8
tranhuutuan261103 <tranhuutuan26112003@gmail.com>
2024-08-03 22:53:28 +0700

    feat: [Cinema Solution] Create a screening

---
+++ CinemaSolution.ViewModels/Screening/ScreeningCreateRequest.cs
@@ -0,0 +1,20 @@
+﻿using CinemaSolution.ViewModels.Auditorium;
+using CinemaSolution.ViewModels.Movie;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaSolution.ViewModels.Screening
+{
+    public class ScreeningCreateRequest
+    {
+        public int Id { get; set; }
+        public int MovieId { get; set; }
+        public DateTime StartDate { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public AuditoriumViewModel Auditorium { get; set;} = new AuditoriumViewModel();
+        public List<SeatDefaultViewModel> SeatDefaults { get; set; } = new List<SeatDefaultViewModel>();
+    }
+}
