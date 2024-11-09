8e58eae920172a7e04dfa4382040cc9c3e29e184
tranhuutuan261103 <tranhuutuan26112003@gmail.com>
2024-11-09 22:17:29 +0700

    feat: [Cinema Solution] Handle errors when updating movie

--- CinemaSolution.ViewModels/Movie/MovieUpdateRequest.cs
+++ CinemaSolution.ViewModels/Movie/MovieUpdateRequest.cs
@@ -3,6 +3,7 @@ using CinemaSolution.ViewModels.Common.ItemSelection;
 using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,12 +13,18 @@ namespace CinemaSolution.ViewModels.Movie
     public class MovieUpdateRequest
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Title is required")]
         public string Title { get; set; } = string.Empty;
         public List<ItemSelection<CategoryViewModel>> Categories { get; set; } = new List<ItemSelection<CategoryViewModel>>();
+        [Required(ErrorMessage = "Language is required")]
         public string Language { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Director is required")]
         public string Director { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Actors is required")]
         public string Actors { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Description is required")]
         public string Description { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Trailer URL is required")]
         public string TrailerUrl { get; set; } = string.Empty;
         public DateTime ReleaseDate { get; set; } = DateTime.Now;
         public DateTime EndDate { get; set; } = DateTime.Now;
