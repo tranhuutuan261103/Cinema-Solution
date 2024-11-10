f03f6d69fbaf4a44f599a7d0ac063de2832c1119
tranhuutuan261103 <tranhuutuan26112003@gmail.com>
2024-11-10 23:35:19 +0700

    feat: [Cinema Solution] Handle errors when crud category

--- CinemaSolution.ViewModels/Category/CategoryCreateRequest.cs
+++ CinemaSolution.ViewModels/Category/CategoryCreateRequest.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace CinemaSolution.ViewModels.Category
 {
     public class CategoryCreateRequest
     {
+        [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; } = string.Empty;
     }
 }
